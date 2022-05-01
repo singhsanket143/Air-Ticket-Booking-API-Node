2e0c25add83426ca7a87a432e96fc0ec508fb5ec
Sanket Singh <sanketsingh@Sankets-MacBook-Pro.local>
2022-05-01 12:03:17 +0530

    Added airline and flight service

---
+++ src/service/airlineService.js
@@ -0,0 +1,47 @@
+const Airline = require('../models/airline');
+
+const createAirline = async (data) => {
+    try {
+        const newAirline = {
+            name: data.name
+        }
+        const response = await new Airline(newAirline).save();
+        return response;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const destroyAirline = async (name) => {
+    try {
+        const response = await Airline.findOneAndDelete({name: name});
+        return response;
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+const getAirline = (name) => {
+    try {
+        const response = await Airline.findOne({name: name});
+        return response;
+    } catch(err) {
+        console.log(err);
+    }
+}   
+
+const getAllAirlines = () => {
+    try {
+        const response = await Airline.find();;
+        return response;
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+module.exports = {
+    createAirline,
+    destroyAirline,
+    getAirline,
+    getAllAirlines
+}
---
+++ src/service/flightService.js
@@ -0,0 +1,65 @@
+const Flight = require('../models/flight');
+
+const createFlight = async (data) => {
+    try {
+        const newFlight = {
+            departureAirport: data.departureAirport,
+            arrivalAirport: data.arrivalAirport,
+            duration: data.duration,
+            flightDate: data.flightDate,
+            departureTime: data.departureTime,
+            arrivalTime: data.arrivalTime,
+            flightNumber: data.flightNumber,
+            price: data.price,
+            airline: data.airlineId
+        }
+        const response = await new Flight(newFlight).save();
+        return response;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const updateFlight = (data) => {
+    try {
+        const response = await Flight.updateOne({flightNumber: data.flightNumber}, data);
+        return response;
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+const destroyFlight = async (flightNumber) => {
+    try {
+        const response = await Flight.findOneAndDelete({flightNumber: flightNumber});
+        return response;
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+const getFlight = (flightNumber) => {
+    try {
+        const response = await Flight.findOne({flightNumber: flightNumber});
+        return response;
+    } catch(err) {
+        console.log(err);
+    }
+}   
+
+const getAllFlights = () => {
+    try {
+        const response = await Flight.find();;
+        return response;
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+module.exports = {
+    createFlight,
+    updateFlight,
+    destroyFlight,
+    getFlight,
+    getAllFlights
+}
